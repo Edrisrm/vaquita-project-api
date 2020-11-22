cdbd0ca972489c04352c592109b0be7c62ce28ed
elvinlab <60045534+elvinlab@users.noreply.github.com>
2020-11-22 11:58:19 -0600

    Update inventory.js

--- models/inventory.js
+++ models/inventory.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const AutoIncrement = require("mongoose-sequence")(mongoose);
 var mongoosePaginate = require("mongoose-paginate-v2");
-
+const moment = require("moment");
+moment.locale("es");
 const InventorySchema = mongoose.Schema({
   breed: {
     type: String,
@@ -24,9 +25,12 @@ const InventorySchema = mongoose.Schema({
     default: "en_finca",
   },
 
-  apart: { type: mongoose.Schema.ObjectId, ref: 'Apart' },
+  apart: { type: mongoose.Schema.ObjectId, ref: "Apart" },
 
-  date: { type: Date, default: Date.now },
+  date: {
+    type: String,
+    default: () => moment().format("DD, MM  YYYY, HH:MM:SS"),
+  },
 });
 
 InventorySchema.plugin(AutoIncrement, {
