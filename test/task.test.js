c3aad10fba3e4f354aec0f5c618ceb4b61df5779
Edrisrm <44209633+Edrisrm@users.noreply.github.com>
2020-11-22 20:22:32 -0600

    pruebas unitarias y nombres nuevos para los yml
    pruebas unitarias y nombres nuevos para los yml

--- test/task.test.js
+++ test/task.test.js
@@ -1,13 +1,13 @@
 var app = require('../express_server'),
   chai = require('chai'),
   request = require('supertest');
-let token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiI1ZmI5MjI0ZGVkZGJmMzM1ZTRiY2U4OGQiLCJlbWFpbCI6ImVkcmlzcmlvc21vcmFsZXNAZ21haWwuY29tIiwiZ2l2ZW5fbmFtZSI6IkVkcmlzIiwiZmFtaWx5X25hbWUiOiJSaW9zIE1vcmFsZXMiLCJyb2xlIjoiUk9MRV9BRE1JTklTVFJBVE9SIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BT2gxNEdnUVNJRmJhdFR5UzN1UHFEVG1tLTZSWUdaMEZtRGwtcHFjTkFRd29RPXM5Ni1jIiwidHdvX2ZhY3RvcnNfYWN0aXZhdGVkIjp0cnVlLCJpYXQiOjE2MDU5NjkxMDJ9.zVuSeHDftFC3V22qF9YsQACGBqLWtrM7MtBiheL5fdA`
+let token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiI1ZmI5MjI0ZGVkZGJmMzM1ZTRiY2U4OGQiLCJlbWFpbCI6ImVkcmlzcmlvc21vcmFsZXNAZ21haWwuY29tIiwiZ2l2ZW5fbmFtZSI6IkVkcmlzIiwiZmFtaWx5X25hbWUiOiJSaW9zIE1vcmFsZXMiLCJyb2xlIjoiUk9MRV9BRE1JTklTVFJBVE9SIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BT2gxNEdnUVNJRmJhdFR5UzN1UHFEVG1tLTZSWUdaMEZtRGwtcHFjTkFRd29RPXM5Ni1jIiwidHdvX2ZhY3RvcnNfYWN0aXZhdGVkIjp0cnVlLCJpYXQiOjE2MDYwOTYwNDR9.8wCZmIXkTMIVeUT26RemuAjIOXE45hKVRJpB0Ja4g_I`
 describe('POST/Inventory', () => {
 
   it('responds with json',(done) =>{
     request(app)
         .post(`/api/agregar-inventario`)
-        .send({breed: "miura", age_in_months: "1 año", status: "en_finca", weight: "200", apartValue: "1"})
+        .send({breed: "miura", age_in_months: "1 año", status: "en_finca", weight: "200", apartValue: "5fbb15e0b4b6852b9fc01c92", image: null})
         .expect(403)
         .end(function(err, res){
             if(err){
@@ -20,11 +20,15 @@ describe('POST/Inventory', () => {
         });
   })
 })
+random_num = Math.random() * (1000 - 1) + 1;
+random_apart_num_cell = Math.round(random_num, 3); 
+square_meter_num_cell = Math.round(random_num, 3);
+
 describe('POST/Apart', () => {
     it('responds with json',(done) =>{
       request(app)
           .post(`/api/agregar-apartado`)
-          .send({square_meter: 120, apart_number: 12})
+          .send({square_meter: square_meter_num_cell, apart_number: random_apart_num_cell})
           .set({ "Authorization": token })
           .expect(200)
           .end(function(err, res){
@@ -87,6 +91,26 @@ describe('GET/Inventory', () => {
           .set('Accept', 'application/json')
           .set({ "Authorization": token })
           .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res){
+              if(err){
+                  console.log(err);
+                  done(err);
+              }
+              else {
+                  done();
+                  return;
+              }
+          });
+    })
+  })
+  weight_num_cell = Math.round(random_num, 3); 
+  describe('PUT/inventory', () => {
+    it('responds with json',(done) =>{
+      request(app)
+          .put(`/api/editar-inventario`)
+          .send({_id:"5fbb15e0b4b6852b9fc01c92",age_in_months: "3 año", weight: weight_num_cell})
+          .set({ "Authorization": token })
           .expect(200)
           .end(function(err, res){
               if(err){
